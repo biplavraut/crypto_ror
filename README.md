59a92cdd00911ec31d9f1c49c25fd6ff171e0671
Biplav Raj Raut <biplavraut@gmail.com>
2021-05-12 22:13:13 +0545

    Summary

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CRYPTOGRAPHY Ruby on Rails
 
-Click and Press
-Application on ROR.
+* Click and Press
+* Application on ROR.
 
-This is a simple app to send message, store it encrypted in database and view the message.
+* This is a simple app to send message, store it encrypted in database and view the message.
+* Lockbox
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,10 +3,12 @@ class ApiController < ApplicationController
     before_action :authorized_key, only: %i[ view ]
     before_action :custom_message, only: %i[ view_message ]
     
+    #view all the data inserted
     def view
         render json: @messages
     end
 
+    # View only message to a key value
     def view_message
         key = Lockbox.generate_key
         lockbox = Lockbox.new(key: key, encode: true)
@@ -28,21 +30,19 @@ class ApiController < ApplicationController
     end
     
     private
+        # Check if the request is authorized
         def authorized_key
-            auth_key = 123456
-            user_key = params[:key]
+            auth_key = 123456 #static key value
+            user_key = params[:key] #user post key value
             if auth_key == user_key
                 @messages = Message.all
+            else
+                render json: @message.errors, status: :unprocessable_entity
             end
-
         end
         # Use callbacks to share common setup or constraints between actions.
-        def set_message
-            @message = Message.find(params[:id])
-        end
-
         def custom_message
-            @message = Message.where(key: params[:key]).first
+            @message = Message.where(key: params[:key]).first #check for key value to decrypt message
         end
         
         # Only allow a list of trusted parameters through.
